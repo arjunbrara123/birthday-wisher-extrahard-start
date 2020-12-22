b9cf65c982f63a663e0f3d5f911a85f8c74198ce
Arjun <arjunbrara@hotmail.co.uk>
2020-12-22 11:39:06 +0000

    Update

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ for row in data.itertuples():
         email = row.email
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
         with open("letter_templates\letter_" + str(random.randint(1, 3)) + ".txt") as file:
             letter = file.read().replace("[NAME]", name)
         msg = "Subject:Happy Birthday!\n\n"+letter
+
 # 4. Send the letter generated in step 3 to that person's email address.
 
         with smtplib.SMTP("smtp.live.com") as connection:
